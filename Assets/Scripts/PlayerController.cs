77b5499e4109b96e87aacb0b96ad9af0222f151e
SungHo Choi <dolphinflow86@gmail.com>
2017-05-05 15:34:10 -0700

    Added playerController and collider

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float playerHP;
+
+	void Start ()
+    {
+
+    }
+	
+	void Update ()
+    {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("cndehf!!!!!!!!!");
+        Destroy(collision.gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("cndehf!!!!!!!!!");
+        Destroy(collision.gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("cndehf!!!!!!!!!");
+        Destroy(other.gameObject);
+    }
+}
