004f2c203326de5c567a78ed3bf52a1a7825f6dd
Jay Park <bbakjj@gmail.com>
2017-05-05 10:45:49 -0700

    Test Scene

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        transform.position = mousePos;
+    }
+}
