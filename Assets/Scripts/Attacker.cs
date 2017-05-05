62aacd8d83dfd54ef25ffe75d41d3e61cef080c3
SungHo Choi <dolphinflow86@gmail.com>
2017-05-05 11:38:00 -0700

    Calc time.

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class Attacker : MonoBehaviour {
 
     public float missileSpeed;
+    public Vector2 xRange;
+    public Vector2 yRange;
+
     private Vector3 playerPos;
     private Vector3 dirVec;
 	void Start ()
     {
         playerPos = FindObjectOfType<Mover>().transform.position;
-
-        Vector3 randomRange = new Vector3(Random.Range(-2, 2), Random.Range(-2, 2), 1);
-
+        Vector3 randomRange = new Vector3(Random.Range(xRange.x, xRange.y), Random.Range(yRange.x, yRange.y), 1);
         dirVec = playerPos - transform.position + randomRange;
-        
     }
 	
 	void Update ()
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
+    public Vector2 spawnMonsterRatio;
+    private float timeRecord;
+
 	void Start ()
     {
-
+        timeRecord = 0.0f;
     }
 
     void Update ()
     {
-
-
-        
+        timeRecord += Time.deltaTime;
 	}
 }
