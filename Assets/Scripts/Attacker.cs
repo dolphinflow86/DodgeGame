cb3a302f2fe73772bbb6c8c8fe481500971aaf45
SungHo Choi <dolphinflow86@gmail.com>
2017-05-05 11:25:21 -0700

    Shoot the missile around player

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour {
+
+    public float missileSpeed;
+    private Vector3 playerPos;
+    private Vector3 dirVec;
+	void Start ()
+    {
+        playerPos = FindObjectOfType<Mover>().transform.position;
+
+        Vector3 randomRange = new Vector3(Random.Range(-2, 2), Random.Range(-2, 2), 1);
+
+        dirVec = playerPos - transform.position + randomRange;
+        
+    }
+	
+	void Update ()
+    {
+        transform.position += dirVec.normalized * missileSpeed;
+	}
+}
