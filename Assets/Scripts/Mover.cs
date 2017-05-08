a85795db793da680fabc681f34e05f580fb1a4f3
SungHo Choi <dolphinflow86@gmail.com>
2017-05-08 10:40:55 -0700

    Player Collider issue resolved.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -12,16 +12,16 @@ public class Mover : MonoBehaviour {
 
     void Update ()
     {
-        //Vector2 sprite_size = GetComponent<SpriteRenderer>().sprite.rect.size;
+        Vector2 sprite_size = GetComponent<SpriteRenderer>().sprite.rect.size;
         Vector2 mousePos = Input.mousePosition;
         //Debug.Log("mousePos : " + mousePos);
-        //Debug.Log("Bounds : " + sprite_size);
+        Debug.Log("Bounds : " + sprite_size.x);
         //Debug.Log("ScreenX : " + Screen.width);
         //Debug.Log("ScreenY : " + Screen.height);
 
         // restrict the mouse position within screen
-        mousePos.x = Mathf.Clamp(mousePos.x, 10.0f, Screen.width - 10.0f);
-        mousePos.y = Mathf.Clamp(mousePos.y, 10.0f, Screen.height - 10.0f);
+        mousePos.x = Mathf.Clamp(mousePos.x, 0.0f + (sprite_size.x * 0.5f), Screen.width - (sprite_size.x * 0.5f));
+        mousePos.y = Mathf.Clamp(mousePos.y, 0.0f, Screen.height);
 
         Vector2 newPosition = Camera.main.ScreenToWorldPoint(mousePos);
         transform.position = new Vector2(newPosition.x, newPosition.y);
