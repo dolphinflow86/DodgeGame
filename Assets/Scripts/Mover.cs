d87234a45253ff466bf2ab2c42dd234e383a0cbb
Jay Park <bbakjj@gmail.com>
2017-05-08 14:01:41 -0700

    Camera boundary

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,24 +4,28 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour {
 
-	void Start ()
+    Vector2 sprite_size;
+    Vector2 local_size;
+
+    void Start()
     {
         Cursor.visible = false;
+        sprite_size = GetComponent<SpriteRenderer>().sprite.rect.size;
+        local_size = gameObject.transform.localScale;
     }
-
 
     void Update ()
     {
-        Vector2 sprite_size = GetComponent<SpriteRenderer>().sprite.rect.size;
+        
         Vector2 mousePos = Input.mousePosition;
         //Debug.Log("mousePos : " + mousePos);
-        Debug.Log("Bounds : " + sprite_size.x);
+        //Debug.Log("Bounds : " + sprite_size.x);
         //Debug.Log("ScreenX : " + Screen.width);
         //Debug.Log("ScreenY : " + Screen.height);
 
         // restrict the mouse position within screen
-        mousePos.x = Mathf.Clamp(mousePos.x, 0.0f + (sprite_size.x * 0.5f), Screen.width - (sprite_size.x * 0.5f));
-        mousePos.y = Mathf.Clamp(mousePos.y, 0.0f, Screen.height);
+        mousePos.x = Mathf.Clamp(mousePos.x, 0.0f + (sprite_size.x * 0.3f * local_size.x), Screen.width - (sprite_size.x * 0.3f * local_size.x));
+        mousePos.y = Mathf.Clamp(mousePos.y, (sprite_size.y * 0.3f * local_size.y), Screen.height - (sprite_size.y * 0.3f * local_size.y));
 
         Vector2 newPosition = Camera.main.ScreenToWorldPoint(mousePos);
         transform.position = new Vector2(newPosition.x, newPosition.y);
