e0b19a34c39dc766fdc297b151b0f7c1badffb6c
Jay Park <bbakjj@gmail.com>
2017-05-10 13:37:38 -0700

    ui background

--- Assets/Scripts/UI/GameoverPopUp.cs
+++ Assets/Scripts/UI/GameoverPopUp.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameoverPopUp : UIScreen {
 
-    public void OnCancelButton()
+    public void OnYesButton()
     {
         ScreenManager.instance.Hide();
+        SceneManager.UnloadSceneAsync(1);
+        SceneManager.LoadScene("Game", LoadSceneMode.Additive);
+
     }
 
     public void OnConfirmButton()
     {
+        //Hide the gameoverPopup
+        ScreenManager.instance.Hide();
         //Hide the pausepopup
         ScreenManager.instance.Hide();
 
