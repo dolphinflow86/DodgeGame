fafa87f615758c06e944b3dcd14adead906e6310
Jay Park <bbakjj@gmail.com>
2017-05-11 14:07:40 -0700

    sound on off 기능 추가

---
+++ Assets/Scripts/UI/SoundOnOff.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundOnOff : MonoBehaviour {
+
+    private bool soundOn;
+    private Image img;
+
+    public Sprite soundOnSprite;
+    public Sprite soundOffSprite;
+
+    private void Start()
+    {
+        img = GetComponent<Image>();
+    }
+
+    private void OnEnable()
+    {
+        if (soundOn)
+        {
+            img.sprite = soundOnSprite;
+            AudioListener.pause = false;
+        }
+        else
+        {
+            img.sprite = soundOffSprite;
+            AudioListener.pause = true;
+        }
+    }
+
+    public void SoundToggle()
+    {
+        soundOn =! soundOn;
+        if(soundOn)
+        {
+            img.sprite = soundOnSprite;
+            AudioListener.pause = false;
+        }
+        else
+        {
+            img.sprite = soundOffSprite;
+            AudioListener.pause = true;
+        }
+    }
+
+}
