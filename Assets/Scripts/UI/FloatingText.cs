6ba1eb2cdaf3e85d49dd214e6da240e1e4fc869f
Jay Park <bbakjj@gmail.com>
2017-05-12 16:07:51 -0700

    working on floating texts

---
+++ Assets/Scripts/UI/FloatingText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatingText : MonoBehaviour {
+
+    Animator animator;
+    Text damageText;
+
+	void Start ()
+    {
+        animator = GetComponentInChildren<Animator>();
+        AnimatorClipInfo[] clipInfo = animator.GetCurrentAnimatorClipInfo(0);
+        Destroy(gameObject, clipInfo[0].clip.length);
+        damageText = animator.GetComponent<Text>();
+	}
+	
+    public void SetText(string text)
+    {
+        damageText.text = text;
+    }
+
+}
---
+++ Assets/Scripts/UI/FloatingTextController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingTextController : MonoBehaviour {
+
+    FloatingText popupText;
+    GameObject canvas;
+
+    FloatingTextController instance;
+
+    private void Awake()
+    {
+        instance = this;
+        canvas = FindObjectOfType<Canvas>().gameObject;
+        popupText = Resources.Load<FloatingText>("Prefabs/PopupText_Parent");
+    }
+
+    public void CreateFloatingText(string text, Transform location)
+    {
+        FloatingText fText = Instantiate(popupText);
+        Vector2 screenPosition = Camera.main.WorldToScreenPoint(location.position);
+        fText.transform.SetParent(canvas.transform, false);
+        fText.SetText(text);
+    }
+
+
+}
